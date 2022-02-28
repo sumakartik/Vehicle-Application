ffd5260b10f90cc25746984e2a882fb112dcbcd7
Suma Kartik <pair+kartiksuma@forcenex.us>
2022-02-28 18:13:09 -0500

    added login modal skeleton

--- vehicle-dictionary/src/components/TopBar.js
+++ vehicle-dictionary/src/components/TopBar.js
@@ -3,9 +3,9 @@ import { styled } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+// import Modal from "@mui/material/Modal";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   color: "transparent",
@@ -17,6 +17,22 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   },
   input: { color: "black" },
 }));
+
+// const style = {
+//   position: "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: 400,
+//   bgcolor: "background.paper",
+//   border: "2px solid #000",
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+// const [open, setOpen] = React.useState(false);
+// const handleOpen = () => setOpen(true);
+// const handleClose = () => setOpen(false);
 
 // const useStyles = makeStyles({
 //   input: {
@@ -41,9 +57,28 @@ export default function TopBar() {
           >
             Welcome to the Vehicle Dictionary
           </Typography>
-          <Button color="inherit" sx={{ color: "black" }}>
+          <Button
+            color="inherit"
+            sx={{ color: "black" }}
+            // onClick={handleOpen}>
+          >
             Login
           </Button>
+          {/* <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal> */}
         </StyledToolbar>
       </AppBar>
     </Box>
